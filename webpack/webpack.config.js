e426caa02161efb65f49d26d56dfa2ea8ae96d30
Unfamiliar-1123 <dqlw1123@126.com>
2020-02-20 11:16:41 +0800

    0220

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,14 @@
 // node引入模块的方式
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 module.exports = {
     mode:"development", //development|production
     // 入口文件 
     entry:"./src/index.js",
     // 出口文件 打包到哪
     output:{
-        filename:"bundle.js",
+        filename:"main.js",
         // path是绝对路径 
         path:path.resolve(__dirname,"dist")
     },
@@ -35,9 +37,20 @@ module.exports = {
                 use:[ //loader的执行顺序从后向前
                     "style-loader",
                     "css-loader",
+                    "postcss-loader",
                     "sass-loader"
                 ]
             }
         ]
-    }
+    },
+    plugins:[
+        // template中的src/indx.html作为模板生成dist目录，并把打包生成的js注入到html中
+        new HtmlWebpackPlugin({
+            template:"./src/index.html"
+        }),
+        // 打包之前先将dist目录删除
+        new CleanWebpackPlugin()
+    ]
 }
+
+
