87cc4e86a29915c31eb7798a2be1eb01949dad1e
Unfamiliar-1123 <dqlw1123@126.com>
2020-02-20 15:19:41 +0800

    ghsu5

---
+++ webpack/index.js
@@ -0,0 +1,14 @@
+// es6引入模块的方式  ./代表当前路径
+import img from './img/21.jpg';
+// import style from './css/index.css';
+import style from './css/test.scss';
+// console.log(q);
+let aaaaaaa = 100;
+let fn = (x,y) =>{
+    console.log(x,y);
+}
+fn(1,2)
+console.log(aaaaaaa);
+var oImg = new Image();
+oImg.src= img;
+document.body.append(oImg);
