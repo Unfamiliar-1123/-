9b003d20c8ec360feeaf553792931e0eea8ec1c0
Unfamiliar-1123 <dqlw1123@126.com>
2020-02-26 14:59:47 +0800

    0226

--- vue/project/src/App.vue
+++ vue/project/src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
-      <common-header></common-header>
-      <home></home>
-      <common-footer></common-footer>
+      <!-- <common-header></common-header> -->
+      <!-- <home></home> -->
+      <!-- <common-footer></common-footer> -->
+      <router-link to="/">首页</router-link> | 
+      <router-link to="/home">首页</router-link> | 
+      <!-- <router-link to="/detail">详情页</router-link> |  -->
+      <!-- <router-link :to="{name:'detail', params:{id:25,name:'xx'}}">详情页</router-link> |  -->
+      <!-- <router-link :to="{path:'/detail', query:{id:25,name:'xx'}}">详情页</router-link> |  -->
+      <router-link to="/detail/25/xx">详情页</router-link> | 
+      <router-link to="/abc">abc</router-link> |
+      <!--占位  -->
+      <router-view/>
   </div>
 </template>
 <script>
@@ -24,7 +33,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
