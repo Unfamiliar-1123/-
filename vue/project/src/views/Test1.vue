0d73b266a9af0ed94c94d55f40832c3c976133b5
Unfamiliar-1123 <dqlw1123@126.com>
2020-02-27 10:20:32 +0800

    test

---
+++ vue/project/src/views/Test1.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <!-- $store.commit('')触发mutations中的方法 -->
+        <button @click="$store.commit('add',5)">count add</button>
+        {{$store.state.count}}
+        {{$store.state.num}}
+    </div>
+</template>
+
+<script>
+    export default {
+        
+       
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
