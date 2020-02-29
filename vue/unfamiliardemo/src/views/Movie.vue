c23b4699f5d184b4e9b03694f6ed4d1e6c5babe5
Unfamiliar-1123 <dqlw1123@126.com>
2020-02-29 10:35:03 +0800

    电影详情

--- vue/unfamiliardemo/src/views/Movie.vue
+++ vue/unfamiliardemo/src/views/Movie.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
         <ul class="list-box">
-            <li class="list" v-for="obj in movieList" :key="obj.id">
+            <li class="list" 
+                v-for="obj in movieList" 
+                :key="obj.id"
+                @click="goDetail(obj.id)"
+            >
                 <img :src="obj.images.medium" alt="">
                 <div class="desc-box">
                     <h3>{{obj.original_title}}</h3>
-                    <div>肖央|</div>
+                    <div>
+                        <span v-for="cast in obj.casts" :key="cast.id">{{cast.name}}|</span>
+                    </div>
                     <p>207147已观看</p>
-                    <p>年份：2020</p>
-                    <p>剧情</p>
+                    <p>年份：2019</p>
+                    <p>
+                        <span v-for="(genre,index) in obj.genres" :key="index">{{genre}}</span>
+                    </p>
                 </div>
             </li>
             
@@ -24,11 +32,16 @@
                 movieList:[]
             }
         },
+        methods:{
+            goDetail(id){
+                this.$router.push({path:'/movie-detail',query:{id}})
+            }
+        },
         
         created(){
             axios.get('https://bird.ioliu.cn/v1?url=https://douban.uieee.com/v2/movie/in_theaters?start=0&count=10')
             .then((res)=>{
-                // console.log(res)
+                // console.log(res.data)
                 this.movieList = res.data.subjects;
             }).catch((res)=>{
                 console.log(res)
---
+++ vue/unfamiliardemo/src/views/MovieDetail.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+电影详情页
+{{$route.query.id}}
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        data(){
+            return {
+                movie:{}
+            }
+        },
+        created(){
+            axios.get('https://bird.ioliu.cn/v1?url=https://douban.uieee.com/v2/movie/subject/'+this.$route.query.id)
+            .then((res)=>{
+                this.movie = res.data;
+            }).catch((res)=>{
+                console.log(res)
+            })
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
