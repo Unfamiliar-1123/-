f3bb91bdcf64d887e37789bc25ef02c2e4ae053c
Unfamiliar-1123 <dqlw1123@126.com>
2020-03-04 14:03:28 +0800

    lalalaa

--- vue/unfamiliardemo/src/components/Swipe.vue
+++ vue/unfamiliardemo/src/components/Swipe.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
-        <ul class="swipe-box">
+        <!-- <ul class="swipe-box"> -->
+        <transition-group tag="ul" name="fade" class="swipe-box">
             <li class="swipe-list" 
                 v-for="(img,index) in imgs" 
                 :key="index"
                 v-show="index == iNow"
             >
+            <!-- v-show="index == iNow" 索引==iNow的元素显示 其他隐藏 -->
                 <img :src="img" alt="">
             </li>
-        </ul>
+        </transition-group>
+        <!-- </ul> -->
         <ul class="btns-box">
             <li 
                 :class="{btns:true,active:iNow == index}" 
@@ -23,14 +26,10 @@
     export default {
         data(){
             return {
-                imgs:[
-                    "https://img3.doubanio.com/lpic/s24468373.jpg",
-                    "https://img3.doubanio.com/lpic/s27102925.jpg",
-                    "https://img3.doubanio.com/lpic/s6989253.jpg"
-                ],
                 iNow:0//记录当前显示的索引
             }
         },
+        props:["imgs"],
         created(){
             setInterval(()=>{
                 this.iNow++;
@@ -44,12 +43,22 @@
 </script>
 
 <style lang="scss" scoped>
-    .swipe-list{
-        img{
+    .swipe-box{
+        height:5rem;
+        position: relative;
+        .swipe-list{
+            position: absolute;
+            left:0px;
+            top:0px;
             width:100%;
-            height:5rem;
+            height: 5rem;
+            img{
+                width:100%;
+                height:5rem;
+            }
         }
     }
+    
     .btns-box{
         display: flex;
         justify-content: center;
@@ -64,5 +73,24 @@
             background: #000;
         }
     }
+    .fade-enter{
+        transform: translate(-100%);
+    }
+    .fade-enter-active{
+        transition: transform 1s linear;
+    }
+    .fade-enter-to{
+        transform: translate(0%);
+    }
+    .fade-leave{
+        transform: translate(0);
+    }
+    .fade-leave-active{
+        transition: transform 1s linear;
+    }
+    .fade-leave-to{
+        transform: translate(100%);
+    }
+    
     
 </style>
