f68621f863169ad974ed0bff44aadd124df7a75c
Unfamiliar-1123 <dqlw1123@126.com>
2020-03-18 13:28:09 +0800

    vueapp

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+   <common-header></common-header>
+    <div class="container"> 
+      465151651
+      </div>
+   <common-footer></common-footer>
+   
   </div>
 </template>
+<script>
+import CommonHeader from'./components/CommonHeader';
+import CommonFooter from'./components/CommonFooter';
+export default {
+  components:{
+    CommonHeader,
+    CommonFooter
+  }
+}
+
+</script>
+
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-size: 12px;
+  
+}
+.container{
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ vueapp/src/components/CommonFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        footer
+    </footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    footer{
+        height: 1rem;
+        background: tan;
+        line-height: 1rem;
+        text-align: center;
+        font-size: 16px;
+        position: fixed;
+        bottom: 0px;
+        width: 100%;
+    }
+</style>
