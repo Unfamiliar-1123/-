051d50d014d7fe572f12812ba0765f591600a48c
Unfamiliar-1123 <dqlw1123@126.com>
2020-03-25 15:35:19 +0800

    VUE

--- vueapp/src/views/MovieDetail.vue
+++ vueapp/src/views/MovieDetail.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
-电影详情
-{{$route.query.id}}
+        <div class="detail-top">
+            <img src="movie.images.medium" alt="">
+            <!-- <img src="http://5b0988e595225.cdn.sohucs.com/images/20191115/dc5981945484440d90249f00f6001b3a.jpeg" alt="picture"> -->
+            <div class="detail-info">
+                <h2>{{movie.original_title}}</h2>
+                <p>{{movie.pubdate}}</p>
+                <p></p>
+            </div>
+        </div>
+        <div class="bottom">
+            电影详情
+        </div>
+<!-- 电影详情
+{{$route.query.id}} -->
     </div>
 </template>
 
@@ -22,5 +34,14 @@
     
 
 <style lang="scss" scoped>
+.detail-top{
+    display: flex;
+    img{
+        width: 3rem;
+    }
+    .detail-info{
+        flex: 1;
+    }
+}
 
 </style>
